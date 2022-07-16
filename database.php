f6ce096fe53006691728db09c8fe35a9c1d6bf7d
Marko <omar.yzdz19@mhs.uinjkt.ac.id>
2022-07-17 00:18:47 +0700

    Initial Commit

--- database.php
+++ database.php
@@ -2,7 +2,7 @@
 	$hostname = 'localhost';
 	$username = 'root';
 	$password = '';
-	$dbname   = 'goinvent';
+	$dbname   = 'ilogi';
 
 
 	$koneksi = mysqli_connect($hostname, $username, $password, $dbname) or die ('Gagal terhubung ke database');
